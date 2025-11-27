8e1a5bbdf0cecb3c5adf8a39e57c5ea95289db9c
Haris-261 <haris200408@gmail.com>
2025-11-27 19:55:05 +0500

    commit ping1

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -8,6 +8,9 @@ const userRouter = router()
 
 userRouter.post('/register-user', registerUser)
 userRouter.post('/login-user', login)
+userRouter.get("/ping", (req, res) => {
+    res.json({msg: "Server is working"})
+})
 userRouter.post('/logout-user', isAuthenticated , logout)
 userRouter.get('/get-profile', isAuthenticated , getProfile)
 userRouter.put('/update-profile', isAuthenticated , uploadAvatar.single("avatar"), updateProfile)
